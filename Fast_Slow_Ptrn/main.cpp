7d6f0201510ac6050091845e6c263181eada3e4f
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2021-08-24 02:00:32 +0000

    Calculate Cycle Length in a Cyclic LL

--- Fast_Slow_Ptrn/main.cpp
+++ Fast_Slow_Ptrn/main.cpp
@@ -1,43 +1,84 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct ListNode {
-    ListNode* next;
-    int data;
-    ListNode(int data) {
-        this->data = data;
-        this->next = 0;
-    }
+	ListNode *next;
+	int data;
+	ListNode(int data) {
+		this->data = data;
+		this->next = 0;
+	}
 };
 
-bool HasCycle(ListNode* head) {
-    ListNode* slow = head;
-    ListNode* fast = head;
+int CalculateLength(ListNode*);
 
-    while(fast != 0) {
-        fast = fast->next;
-        if(fast) {
-            fast = fast->next;
-        }
-        slow = slow->next;
-        if(slow == fast) return true;
+bool HasCycle(ListNode *head) {
+	ListNode *slow = head;
+	ListNode *fast = head;
+
+	while (fast != 0) {
+		fast = fast->next;
+		if (fast) {
+			fast = fast->next;
+		}
+		slow = slow->next;
+		if (slow == fast)
+			return true;
+	}
+	return false;
+}
+
+int CycleLength(ListNode* head) {
+  //detect cycle, then call for calculate length
+  ListNode* slow = head;
+  ListNode* fast = head;
+  while(fast) {
+    fast = fast->next;
+    if(fast) {
+      fast = fast->next;
     }
-    return false;
+    slow = slow->next;
+    if(slow == fast) {
+      return CalculateLength(slow);
+    }
+  }
+  return -1;
+}
+
+int CalculateLength(ListNode* slow) {
+  ListNode* ptr = slow;
+  int length = 0;
+  do {
+    length++;
+    ptr = ptr->next;
+  }while(ptr != slow);
+  return length;
+}
+
+ListNode *CreateCyclicList() {
+	ListNode *head = new ListNode(1);
+	head->next = new ListNode(2);
+	head->next->next = new ListNode(3);
+	head->next->next->next = new ListNode(4);
+	head->next->next->next->next = new ListNode(5);
+	head->next->next->next->next->next = new ListNode(6);
+	cout << "LinkedList has cycle: " << HasCycle(head) << endl;
+
+	head->next->next->next->next->next->next = head->next->next;
+	cout << "LinkedList has cycle: " << HasCycle(head) << endl;
+
+	head->next->next->next->next->next->next = head->next->next->next;
+	cout << "LinkedList has cycle: " << HasCycle(head) << endl;
+	return head;
 }
 
 int main() {
-    ListNode *head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
-    head->next->next->next->next->next = new ListNode(6);
-    cout << "LinkedList has cycle: " << HasCycle(head) << endl;
-
-    head->next->next->next->next->next->next = head->next->next;
-    cout << "LinkedList has cycle: " << HasCycle(head) << endl;
-
-    head->next->next->next->next->next->next = head->next->next->next;
-    cout << "LinkedList has cycle: " << HasCycle(head) << endl;
+	ListNode *head = CreateCyclicList();
+  int cycleLen = CycleLength(head);
+  if(cycleLen == -1) {
+    cout << "No cycle detected" << endl;
+  } else {
+    cout << "Cycle of length of " << cycleLen << " detected." << endl;
+  }
+  return 0;
 }
-
